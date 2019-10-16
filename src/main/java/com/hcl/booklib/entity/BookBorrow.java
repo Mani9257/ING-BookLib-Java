baa848b2923bcfe851f968c1332f459e14b6db5e
User1 <User1@LP-5CD9296CLZ.HCLT.CORP.HCL.IN>
2019-10-16 16:13:37 +0530

    Setup

---
+++ src/main/java/com/hcl/booklib/entity/BookBorrow.java
@@ -0,0 +1,32 @@
+package com.hcl.booklib.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class BookBorrow implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer borrowId;
+	private Integer bookId;
+	private Integer userId;
+	private String bookName;
+	private String borrowStatus;
+	private Integer daysLeft;
+	
+}
+
---
+++ src/main/java/com/hcl/booklib/util/ExceptionConstants.java
@@ -0,0 +1,5 @@
+package com.hcl.booklib.util;
+
+public class ExceptionConstants {
+	
+}
